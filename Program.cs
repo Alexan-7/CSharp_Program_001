364d4bd00bfefb57d949a417b207a440f8f3387d
Sasha <a40546b@gmail.com>
2022-08-31 18:18:07 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Insert your name!");
+String username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
